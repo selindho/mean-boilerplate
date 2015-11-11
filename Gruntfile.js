61efeb29ce91f60092ea2f774e0eb4060b48b572
Sebastian Lindholm <sebastian.lindholm@hibox.fi>
2015-11-11 21:31:05 +0200

    Added grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,6 @@
+module.exports = function(grunt) {
+  // A very basic default task.
+  grunt.registerTask('default', 'DefaultTask', function() {
+    grunt.log.write('Grunt default task. ').ok();
+  });
+};
