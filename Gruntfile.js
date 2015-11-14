04d5067c539c2dcd50fad38decbaff6f4c2ff2f5
Sebastian Lindholm <sebastian.lindholm@hibox.fi>
2015-11-14 23:39:25 +0200

    Added client build tasks to Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,53 +2,134 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
+  grunt.registerTask('makeClient', ['clean', 'jshint:clientJavascript', 'uglify:clientJavascript', 'sass', 'cssmin', 'clean:temp']);
+  grunt.registerTask('makeClientDev', ['clean', 'jshint:clientJavascript', 'concat:clientJavascript', 'sass', 'copy:clientJavascript', 'copy:clientStylesheets', /*'clean:temp'*/]);
   grunt.registerTask('default', ['jshint']);
 
   grunt.initConfig({
 
-    jshint: {
-      files: ['Gruntfile.js', 'public/javascripts/**/*.js', 'routes/**/*.js', 'test/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true,
-          _: true
-        }
-      }
-    },
-
     watch: {
-
       gruntfile: {
         files: 'Gruntfile.js',
         tasks: ['default']
       },
 
-      javascript: {
-        files: 'public/javascripts/**/*.js',
+      clientJavascript: {
+        files: 'client/javascripts/**/*.js',
         tasks: ['default']
       },
 
-      stylesheets: {
-        files: 'public/stylesheets/**/*.scss',
+      clientStylesheets: {
+        files: 'client/stylesheets/**/*.scss',
         tasks: ['default']
       },
 
-      routes: {
-        files: 'routes/**/*.js',
+      serverJavascript: {
+        files: ['routes/**/*.js', 'test/**/*.js', 'models/**/*.js' ],
         tasks: ['default']
       },
 
-      test: {
-        files: 'test/**/*.js',
-        tasks: ['default']
-      },
-
-      views: {
+      serverViews: {
         files: 'views/**/*.ejs',
         tasks: ['default']
       }
+    },
 
+    jshint: {
+      clientJavascript: {
+        options: {
+          globals: {
+            jQuery: true,
+            _: true
+          }
+        },
+        files: {
+          src: ['Gruntfile.js', 'client/javascripts/**/*.js', 'routes/**/*.js', 'test/**/*.js']
+        }
+      },
+      serverJavascript: {
+        options: {
+          globals: {
+            _: true
+          }
+        },
+        files: {
+            src: ['Gruntfile.js', 'routes/**/*.js', 'test/**/*.js', 'models/**/*.js']
+        }
+      }
+    },
+
+    uglify: {
+      clientJavascript: {
+        options: {
+            mangleProperties: true,
+            reserveDOMCache: true
+        },
+        files: {
+          'public/build/client/javascripts/script.js': ['client/javascripts/**/*.js']
+        }
+      }
+    },
+
+    clean: {
+        clientBuild: ['public/build'],
+        temp: ['temp']
+    },
+
+    sass: {
+      clientStylesheets: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'temp/client/stylesheets/style.css': ['client/stylesheets/style.scss']
+        }
+      }
+    },
+
+    cssmin: {
+      clientStylesheets: {
+        files: {
+          'public/build/client/stylesheets/style.css': ['temp/client/stylesheets/style.css']
+        }
+      }
+    },
+
+    copy: {
+      clientJavascript: {
+        files: [
+          {
+            expand: true,
+            cwd: 'temp',
+            src: ['client/javascripts/*'],
+            dest: 'public/build'
+          }
+        ],
+      },
+      clientStylesheets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'temp',
+            src: ['client/stylesheets/*'],
+            dest: 'public/build'
+          }
+        ],
+      }
+    },
+
+    concat: {
+      clientJavascript: {
+        src: 'client/javascripts/**/*.js',
+        dest: 'temp/client/javascripts/script.js'
+      }
     }
 
   });
